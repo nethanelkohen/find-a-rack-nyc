8cb3ad8839083edb672a1dd1f1f39d52be187bab
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-08-23 16:56:50 -0400

    updated data, bootstrapped markers

--- App.js
+++ App.js
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { MapView } from 'expo';
 import { ListItem } from 'react-native-elements';
 
-import data from './assets/bikeracks.json';
+import data from './assets/bikeracks-old.json';
 
-export default class App extends React.Component {
-  keyExtractor = (item, index) => index.toString();
+const racks = [
+  {
+    name: '1 7 AV S',
+    Address: '1 7 AV S, New York, NY 10014, USA',
+    value: '5 racks',
+    'coordinate': {
+      'latitude': 40.729711,
+      'longitude': -74.004858
+    }
+  },
+  {
+    name: '1 BOERUM PL',
+    Address: '1 BOERUM PL, Brooklyn, NY 11201, USA',
+    value: '1 rack',
+    'coordinate': { 'latitude': 40.691758, 'longitude': -73.988892 }
+  },
+  {
+    name: '1 CENTRE ST',
+    Address: '1 CENTRE ST, New York, NY 10007, USA',
+    value: '10 racks',
+    coordinate: { 'latitude': 40.712931, 'longitude': -74.003748 }
+  }
+];
 
-  renderItem = ({ item }) => (
-    console.log(item), <ListItem title={item.Name} subtitle={item.Value} />
-  );
+class App extends Component {
+  state = {
+    racks: [data]
+  };
+  // keyExtractor = (item, index) => index.toString();
+  //
+  // renderItem = ({ item }) => (
+  //   console.log(item), <ListItem title={item.Name} subtitle={item.Value} />
+  // );
+
+ 
 
   render() {
     return (
-      <FlatList
-        keyExtractor={this.keyExtractor}
-        data={data}
-        renderItem={this.renderItem}
-      />
+      // <FlatList
+      //   keyExtractor={this.keyExtractor}
+      //   data={data}
+      //   renderItem={this.renderItem}
+      // />
+      <View style={styles.container}>
+        <MapView
+          initialRegion={{
+            latitude: 40.6872281,
+            longitude: -73.9783627,
+            latitudeDelta: 0.2,
+            longitudeDelta: 0.2
+          }}
+          showsUserLocation={true}
+          style={styles.map}
+        >
+          {racks.map((marker, i) => (
+            <MapView.Marker
+              coordinate={marker.coordinate}
+              title={marker.name}
+              description={marker.value}
+              key={i}
+            />
+          ))}
+        </MapView>
+      </View>
     );
   }
 }
@@ -28,5 +79,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
   }
 });
+
+export default App;
