abc29f9e15acd93cc015e90c5d3e88f730c15f65
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-08-24 22:05:01 -0400

    marker clusters, updated json file

--- App.js
+++ App.js
@@ -1,46 +1,78 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { MapView } from 'expo';
-import { ListItem } from 'react-native-elements';
+import { Marker, Callout } from 'react-native-maps';
+import ClusteredMapView from 'react-native-maps-super-cluster';
 
-import racks from './assets/bikeracks.json';
+import racks from './assets/racks.json';
 
 class App extends Component {
-  // keyExtractor = (item, index) => index.toString();
-  //
-  // renderItem = ({ item }) => (
-  //   console.log(item), <ListItem title={item.Name} subtitle={item.Value} />
-  // );
+  renderCluster = (cluster, onPress) => {
+    const pointCount = cluster.pointCount,
+      coordinate = cluster.coordinate,
+      clusterId = cluster.clusterId;
+
+    const clusteringEngine = this.map.getClusteringEngine(),
+      clusteredPoints = clusteringEngine.getLeaves(clusterId, 100);
+
+    return (
+      <Marker coordinate={coordinate}>
+        <View style={{ flex: 1 }}>
+          <Text>{pointCount}</Text>
+        </View>
+      </Marker>
+    );
+  };
+
+  renderMarker = data => (
+    <Marker key={Math.random()} coordinate={data.location} />
+  );
 
   render() {
     return (
-      // <FlatList
-      //   keyExtractor={this.keyExtractor}
-      //   data={racks}
-      //   renderItem={this.renderItem}
-      // />
-      <View style={styles.container}>
-        <MapView
-          initialRegion={{
-            latitude: 40.6872281,
-            longitude: -73.9783627,
-            latitudeDelta: 0.2,
-            longitudeDelta: 0.2
-          }}
-          showsUserLocation={true}
-          style={styles.map}
-        >
-          {racks.map((marker, i) => (
-            <MapView.Marker
-              coordinate={marker.coordinate}
-              title={marker.name}
-              description={marker.value}
-              key={i}
-            />
-          ))}
-        </MapView>
-      </View>
+      <ClusteredMapView
+        style={{ flex: 1 }}
+        data={racks}
+        initialRegion={{
+          latitude: 40.6872281,
+          longitude: -73.9783627,
+          latitudeDelta: 0.2,
+          longitudeDelta: 0.2
+        }}
+        ref={r => {
+          this.map = r;
+        }}
+        renderMarker={this.renderMarker}
+        renderCluster={this.renderCluster}
+      />
+
+      // <View>
+      //   <Text>Hello</Text>
+      // </View>
     );
+    // return (
+    //   <View style={styles.container}>
+    //     <MapView
+    // initialRegion={{
+    //   latitude: 40.6872281,
+    //   longitude: -73.9783627,
+    //   latitudeDelta: 0.2,
+    //   longitudeDelta: 0.2
+    // }}
+    //       showsUserLocation={true}
+    //       style={styles.map}
+    //     >
+    //       {racks.map((marker, i) => (
+    //         <MapView.Marker
+    //           coordinate={marker.coordinate}
+    //           title={marker.name}
+    //           description={marker.value}
+    //           key={i}
+    //         />
+    //       ))}
+    //     </MapView>
+    //   </View>
+    // );
   }
 }
 
