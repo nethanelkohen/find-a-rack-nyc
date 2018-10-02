f3bf4768c46f2c89b7d0335a59074009291c5fb1
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-10-02 01:30:17 -0400

    basic front end mvp

--- App.js
+++ App.js
@@ -1,19 +1,92 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, FlatList, AlertIOS } from "react-native";
+import { StyleSheet, View, Text, AlertIOS, Alert } from "react-native";
 import { MapView } from "expo";
-import { Marker, Callout } from "react-native-maps";
+import { Marker } from "react-native-maps";
 import ClusteredMapView from "react-native-maps-super-cluster";
 
 import racks from "./assets/racks.json";
+
+const LATITUDE_DELTA = 0.04;
+const LONGITUDE_DELTA = 0.04;
+
+const initialRegion = {
+  latitude: 40.7147077,
+  longitude: -74.1443402,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       racks,
-      markerText: null
+      markerText: null,
+      ready: true
     };
   }
+
+  setRegion(region) {
+    let { ready } = this.state;
+    if (ready) {
+      setTimeout(() => this.map.mapview.animateToRegion(region), 10);
+    }
+  }
+
+  componentDidMount() {
+    this.getCurrentPosition();
+  }
+
+  getCurrentPosition() {
+    try {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const region = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          };
+          this.setRegion(region);
+        },
+        error => {
+          switch (error.code) {
+            case 1:
+              if (Platform.OS === "ios") {
+                Alert.alert(
+                  "",
+                  "To locate your location enable permission for the application in Settings - Privacy - Location"
+                );
+              } else {
+                Alert.alert(
+                  "",
+                  "To locate your location enable permission for the application in Settings - Apps - ExampleApp - Location"
+                );
+              }
+              break;
+            default:
+              Alert.alert("", "There was an error fetching your location");
+          }
+        }
+      );
+    } catch (e) {
+      Alert.alert(e.message || "");
+    }
+  }
+
+  onMapReady = e => {
+    if (!this.state.ready) {
+      this.setState({ ready: true });
+    }
+  };
+
+  onRegionChange = region => {
+    // console.log("onRegionChange", region);
+  };
+
+  onRegionChangeComplete = region => {
+    // console.log("onRegionChangeComplete", region);
+  };
 
   renderCluster = (cluster, onPress) => {
     const pointCount = cluster.pointCount,
@@ -38,88 +111,59 @@ class App extends Component {
       coordinate={data.location}
       title={data.name}
       description={data.value}
-      // onPress={() => console.log(onPress)}
     />
   );
 
   takeInCoord = event => {
-    AlertIOS.prompt("How many racks?", null, text =>
-      this.setState({
-        markerText: text
-      })
-    );
-
-    const object = {
-      location: {
-        latitude: null,
-        longitude: null
-      },
-      name: "user-generated marker",
-      value: this.state.markerText
-    };
-    object.location.latitude = event.latitude;
-    object.location.longitude = event.longitude;
-
-    this.setState({ racks: [...this.state.racks, object] });
+    AlertIOS.prompt("How many racks?", null, text => {
+      let textCheck = Number(text);
+      if (isNaN(textCheck)) {
+        Alert.alert("Please enter a number.");
+        return;
+      }
+      this.setState(
+        {
+          markerText: text
+        },
+        () => {
+          const object = {
+            location: {
+              latitude: null,
+              longitude: null
+            },
+            name: "USER-GENERATED MARKER",
+            value: `${this.state.markerText} racks`
+          };
+          object.location.latitude = event.latitude;
+          object.location.longitude = event.longitude;
+          this.setState({ racks: [...this.state.racks, object] });
+        }
+      );
+    });
   };
 
   render() {
-    console.log(this.state.markerText);
     return (
       <ClusteredMapView
-        style={{ flex: 1 }}
-        showsUserLocation={true}
+        showsUserLocation
         data={this.state.racks}
-        initialRegion={{
-          latitude: 40.6872281,
-          longitude: -73.9783627,
-          latitudeDelta: 0.001,
-          longitudeDelta: 0.001
-        }}
+        onMapReady={this.onMapReady}
+        initialRegion={initialRegion}
+        onRegionChange={this.onRegionChange}
+        onRegionChangeComplete={this.onRegionChangeComplete}
         ref={r => {
           this.map = r;
         }}
         renderMarker={this.renderMarker}
         renderCluster={this.renderCluster}
         onLongPress={event => this.takeInCoord(event.nativeEvent.coordinate)}
+        style={StyleSheet.absoluteFill}
       />
     );
-    // return (
-    //   <View style={styles.container}>
-    //     <MapView
-    // initialRegion={{
-    //   latitude: 40.6872281,
-    //   longitude: -73.9783627,
-    //   latitudeDelta: 0.2,
-    //   longitudeDelta: 0.2
-    // }}
-    //       showsUserLocation={true}
-    //       style={styles.map}
-    //     >
-    //       {racks.map((marker, i) => (
-    //         <MapView.Marker
-    //           coordinate={marker.coordinate}
-    //           title={marker.name}
-    //           description={marker.value}
-    //           key={i}
-    //         />
-    //       ))}
-    //     </MapView>
-    //   </View>
-    // );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject
-  },
   clusterContainer: {
     width: 35,
     height: 35,
